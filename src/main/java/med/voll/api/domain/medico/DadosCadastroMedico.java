48d4badf95b623c66580f8487386317e00741c10
tbsilveira <tiagobsilveira@gmail.com>
2023-03-15 22:54:24 +0000

    exception handler created

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,21 +1,23 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
-public record DadosCadastroPaciente(
-        @NotNull
+public record DadosCadastroMedico(
+        @NotBlank
         String nome,
-        @NotNull @Email
+        @NotBlank @Email
         String email,
         @NotBlank @Pattern(regexp = "\\d{6,10}")
         String telefone,
-        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
-        String cpf,
+        @NotBlank @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
         @NotNull @Valid
         DadosEndereco endereco) {
 }
--- src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -1,23 +1,21 @@
-package med.voll.api.medico;
+package med.voll.api.domain.paciente;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(
-        @NotBlank
+public record DadosCadastroPaciente(
+        @NotNull
         String nome,
-        @NotBlank @Email
+        @NotNull @Email
         String email,
         @NotBlank @Pattern(regexp = "\\d{6,10}")
         String telefone,
-        @NotBlank @Pattern(regexp = "\\d{4,6}")
-        String crm,
-        @NotNull
-        Especialidade especialidade,
+        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        String cpf,
         @NotNull @Valid
         DadosEndereco endereco) {
 }
